e9cacde6ab3590d792d8cc17971f3514f17ed7ae
lilian <1220795758@qq.com>
2022-06-28 21:31:18 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    啦啦啦
   </div>
-  <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+const a = 1
+if (a > 0) {
+  const res = 1
+  console.log(res)
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="less" scoped>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
